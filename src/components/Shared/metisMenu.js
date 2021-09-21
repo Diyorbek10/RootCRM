dcaac27ddc1e2107728eae770a8bb9bb46b37ee8
Diyorbek <diyorbeknever@gmail.com>
2021-09-21 13:09:28 +0500

    first commit

---
+++ src/components/Shared/metisMenu.js
@@ -0,0 +1,139 @@
+export const adminMenu = [
+	{
+		"id": 1,
+		"icon": "gear",
+		"label": "Sozlamalar",
+		"to": "/payments"
+	},
+	{
+		"id": 2,
+		"icon": "list-ul",
+		"label": "Jurnallar",
+		"to": "/taskboard"
+	},
+	{
+		"id": "Extra",
+		"label": "To'lovlar",
+	},
+	{
+		"id": 1,
+		"icon": "credit-card",
+		"label": "Barcha to'lovlar",
+		"to": "/payments"
+	},
+	{
+		"id": 4,
+		"icon": "truck",
+		"label": "Harajatlar",
+		"to": "/transports"
+	},
+	{
+		"id": 5,
+		"icon": "calendar-check-o",
+		"label": "Ish haqi",
+		"to": "/attandance"
+	},
+	{
+		"id": 6,
+		"icon": "flag",
+		"label": "To'lovlar guruhi",
+		"to": "/leave"
+	},
+	{
+		"id": 7,
+		"icon": "credit-card",
+		"label": "Kurs to'lovlari",
+		"to": "/setting"
+	},
+	
+]
+export const universityMenu = [
+	{
+		"id": 1,
+		"icon": "dashboard",
+		"label": "Bosh sahifa",
+		"to": "/"
+	},
+	{
+		"id": 2,
+		"icon": "black-tie",
+		"label": "O'qituvchilar",
+		"to": "/professors"
+	},
+	{
+		"id": 3,
+		"icon": "user-circle-o",
+		"label": "Hodimlar",
+		"to": "/staff"
+	},
+	{
+		"id": 4,
+		"icon": "users",
+		"label": "O'quvchilar",
+		"to": "/students"
+	},
+	{
+		"id": 5,
+		"icon": "users",
+		"label": "Guruhlar",
+		"to": "/departments"
+	},
+	{
+		"id": 6,
+		"icon": "graduation-cap",
+		"label": "Kurslar",
+		"to": "/courses"
+	},
+	// {
+	// 	"id": 7,
+	// 	"icon": "book",
+	// 	"label": "Library",
+	// 	"to": "/library"
+	// },
+	// {
+	// 	"id": 8,
+	// 	"icon": "bullhorn",
+	// 	"label": "Holiday",
+	// 	"to": "/holiday"
+	// },
+	// {
+	// 	"id": "Extra",
+	// 	"label": "Extra",
+	// },
+	// {
+	// 	"id": 10,
+	// 	"icon": "calendar",
+	// 	"label": "Calender",
+	// 	"to": "/events"
+	// },
+	// {
+	// 	"id": 11,
+	// 	"icon": "comments-o",
+	// 	"label": "Chat App",
+	// 	"to": "/chat"
+	// },
+	// {
+	// 	"id": 12,
+	// 	"icon": "address-book",
+	// 	"label": "Contact",
+	// 	"to": "/contact"
+	// },
+	// {
+	// 	"id": 13,
+	// 	"icon": "folder",
+	// 	"label": "FileManager",
+	// 	"to": "/fileManager"
+	// },
+	// {
+	// 	"id": 14,
+	// 	"icon": "map",
+	// 	"label": "OurCentres",
+	// 	"to": "/ourCentre"
+	// },
+	// {
+	// 	"id": 15,
+	// 	"icon": "camera-retro",
+	// 	"label": "Gallery",
+	// 	"to": "/gallery"
+	// },
+];
